3571e6f5c1fe5832e053837b95aaa3f0b2d85413
DayYearth <trungquang210102@gmail.com>
2023-10-28 15:54:25 +0700

    aims project

--- Lab02/AimsProject/src/AimsProject/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/AimsProject/DigitalVideoDisc.java
@@ -7,6 +7,38 @@ public class DigitalVideoDisc {
     private int length;
     private float cost;
 
+    //section 16
+    /*
+    ● Classifier/Class member:
+        ○ Defined in a class of which a single copy exists regardless of how many instances of the
+        class exist.
+        ○ Objective: to have variables that are common to all objects
+        ○ Any object of class can change the value of a class variable; that's why you should always
+        be careful with the side effect of class member
+        ○ Class variables can be manipulated without creating an instance of the class
+    ● Instance/Object member:
+        ○ Associated with only objects
+        ○ Defined inside the class but outside of any method
+        ○ Only initialized when the instance is created
+        ○ Their values are unique to each instance of a class
+        ○ Lives as long as the object does
+     */
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getNbDigitalVideoDiscs(){
+        return nbDigitalVideoDiscs;
+    }
+    public void setId(){
+        id = getNbDigitalVideoDiscs();
+        nbDigitalVideoDiscs++;
+    }
+
     public String getTitle(){
         return title;
     }
