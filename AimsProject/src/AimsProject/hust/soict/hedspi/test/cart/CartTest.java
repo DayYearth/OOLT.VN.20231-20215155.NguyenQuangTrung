20871c489fcf10c98104bc00c77868699c278d1c
DayYearth <trungquang210102@gmail.com>
2023-11-30 17:22:46 +0700

    OOP

--- AimsProject/src/AimsProject/hust/soict/hedspi/test/cart/CartTest.java
+++ AimsProject/src/AimsProject/hust/soict/hedspi/test/cart/CartTest.java
@@ -10,18 +10,18 @@ public class CartTest {
         // Create new dvd Object and add them to the Cart
         // Create new dvd1
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        cart.addDigitalVideoDisc(dvd1);
+        cart.addMedia(dvd1);
 
         // Create new dvd2
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        cart.addDigitalVideoDisc(dvd2);
+        cart.addMedia(dvd2);
 
         // Create new dvd3
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        cart.addDigitalVideoDisc(dvd3);
+        cart.addMedia(dvd3);
 
         // Test the method print Cart
-        cart.printNewCart();
+        cart.printCart();
         // To-do: Test the search methods here
     }
 }
