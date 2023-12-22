e9ad56da324e0d6762c9ebdff38431b5c06cbc6f
Quang Trung <147702642+DayYearth@users.noreply.github.com>
2023-12-22 14:34:33 +0700

    Update the Store class to work with Media

--- JavaOOP/AIMSProject/src/hust/soict/hedspi/aims/store/Store.java
+++ JavaOOP/AIMSProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,49 +1,40 @@
 package hust.soict.hedspi.aims.store;
 
 import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+import hust.soict.hedspi.aims.media.Media;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Store {
     public static final int MAX_ITEM_IN_STORE = 1000;
-    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_ITEM_IN_STORE];
     private int nextAvailableIndex;
+    private List<Media> itemsInStore = new ArrayList<>();
+
 
     public Store(){
         nextAvailableIndex = 0;
     }
 
-    public void addDVD(DigitalVideoDisc disc) {
-        if (nextAvailableIndex < itemsInStore.length) {
-            itemsInStore[nextAvailableIndex++] = disc;
-            System.out.println("Added DVD to the store.");
+    public void addMedia(Media media) {
+        itemsInStore.add(media);
+        System.out.println("The media has been added to the store.");
+    }
+
+    public void removeMedia(Media media) {
+        if (itemsInStore.remove(media)) {
+            System.out.println("The media has been removed from the store.");
         } else {
-            System.out.println("The store is full. Cannot add more DVDs.");
+            System.out.println("Media not found in the store. Remove failed.");
         }
     }
 
-    public void removeDVD(DigitalVideoDisc disc) {
-        int indexToRemove = -1;
-        for (int i = 0; i < nextAvailableIndex; i++) {
-            if (itemsInStore[i] == disc) {
-                indexToRemove = i;
-                break;
-            }
+    // Display the list of items in the store
+    public void listMedia() {
+        System.out.println("*************** Items in the Store ***************");
+        for (Media media : itemsInStore) {
+            System.out.println(media.toString());
         }
-
-        if (indexToRemove != -1) {
-            for (int i = indexToRemove; i < nextAvailableIndex - 1; i++) {
-                itemsInStore[i] = itemsInStore[i + 1];
-            }
-            itemsInStore[--nextAvailableIndex] = null;
-            System.out.println("Removed DVD from the store.");
-        } else {
-            System.out.println("DVD not found in the store. Remove failed.");
-        }
-    }
-
-    public void listItemsInStore() {
-        System.out.println("DVDs available in the store:");
-        for (int i = 0; i < nextAvailableIndex; i++) {
-            System.out.println(itemsInStore[i]);
-        }
+        System.out.println("***************************************************");
     }
 }
