e6f9eabc27017ca7f1aae401403d9fb6ee91a8bc
Quang Trung <147702642+DayYearth@users.noreply.github.com>
2023-12-22 09:15:48 +0700

    Create Cart class

---
+++ JavaOOP/Lab02/AIMSProject/src/Cart.java
@@ -0,0 +1,44 @@
+public class Cart {
+    public static final int MAX_NUMBERS_ORDER = 20;
+    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDER];
+    private int qtyOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        if (qtyOrdered == MAX_NUMBERS_ORDER) {
+            System.out.println("The cart is almost full");
+            return;
+        }
+
+        qtyOrdered++;
+        itemOrdered[qtyOrdered] = disc;
+        System.out.println("The disc has been added");
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc){
+        int idToMove = -1;
+        for (int i = 0; i < qtyOrdered; i++){
+            if (itemOrdered[i] == disc){
+                idToMove = i;
+                break;
+            }
+        }
+        if (idToMove < 0) {
+            System.out.println("System cannot find the disc, remove fail");
+            return;
+        }
+        for (int i = idToMove; i < qtyOrdered - 1; i++){
+            itemOrdered[i] = itemOrdered[i + 1];
+        }
+        qtyOrdered--;
+        System.out.println("The disc has been removed");
+    }
+
+    public float totalCost(){
+        float sum = 0;
+        for (int i = 0; i < qtyOrdered; i++){
+            sum += itemOrdered[i].getCost();
+        }
+
+        return sum;
+    }
+}
