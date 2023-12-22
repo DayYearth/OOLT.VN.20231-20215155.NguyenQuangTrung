e3674950db4aa19b6c0b7c532bf082d7c62e4871
Quang Trung <147702642+DayYearth@users.noreply.github.com>
2023-12-22 14:04:12 +0700

    Implement Book class

---
+++ JavaOOP/AIMSProject/src/hust/soict/hedspi/aims/media/Book.java
@@ -0,0 +1,70 @@
+package hust.soict.hedspi.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private List<String> authors;
+
+    public Book() {
+        this.authors = new ArrayList<>();
+    }
+
+    public Book(int id, String title, String category, float cost) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.authors = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void addAuthor(String authorName) {
+        if (!authors.contains(authorName)) {
+            authors.add(authorName);
+        }
+    }
+
+    public void removeAuthor(String authorName) {
+        authors.remove(authorName);
+    }
+}
